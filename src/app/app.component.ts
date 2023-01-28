22427d5e74a4937908ecddaf22e1d141cd58658e
Student <student@itstep.ge>
2023-01-28 18:46:07 +0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular3';
+  firstName:String = "Andria";
+  lastName:String = "Konstantinidi";
+  age:Number = 13;
+  img:String = "https://th.bing.com/th/id/OIP.RiNmH5AIoZwVGvB835DNuAHaE8?pid=ImgDet&rs=1"
+  userName:String = '';
+  onBtnClick(){
+    console.log("btn was clicked")
+  }
 }
